d000fdc3bafc0b217509a41f4567af4702225d6f
David Eliason <david@davethemaker.com>
2018-06-13 15:43:40 -0700

    create Link, Route and Component to test it out

--- src/App.js
+++ src/App.js
@@ -1,21 +1,91 @@
-import React, {Component} from 'react';
-import Router from 'react-router';
+import React, {Component} from 'react'
 
-class App extends React.Component {
+import Buttons from './Buttons'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <h3>welcome</h3>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <h3>me</h3>
+  </div>
+)
+
+class Info extends React.Component {
   render() {
-
     return (
-        <div>
-           <h3> SPA </h3>
-           <ul>
-              <li> Home</li>
-              <li> Stuff </li>
-              <li> Contact</li>
-           </ul>
-           <div className="content"></div>
-        </div>
-      );
+      <div>
+          <h3>Lots of Info</h3>
+      </div>
+    );
   }
 }
 
-export default App; 
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:topicId`} component={Topic}/>
+    <Route exact path={match.path} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+        <li><Link to="/info">Info</Link></li>
+        <li><Link to="/buttons">Buttons</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+      <Route path="/info" component={Info}/>
+      <Route path="/buttons" component={Buttons}/>
+    </div>
+  </Router>
+)
+export default App
